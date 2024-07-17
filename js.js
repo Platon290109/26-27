fe3f9e7958fdeca4f3b6386795ba2f7c0ea98f74
Platon290109 <platon01.2009@gmail.com>
2024-07-17 20:15:00 +0300

    homework sigma

---
+++ js.js
@@ -0,0 +1,12 @@
+let age=15
+console.log(age);
+const name="платон"
+console.log(name);
+let isStudent="true"
+console.log(isStudent);
+const myString="Справжній герой слав, це не той, хто в сяючих обладунках та гарній позі. Герой – той, хто робить, що треба зробити. І забруднитись доводиться, і кров проливати, а він все одно робить, бо так треба"
+console.log(myString);
+let myNumber=12 + 10
+console.log(myNumber);
+const myNull=null
+console.log(myNull);
